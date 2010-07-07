f13982b19e6a56bd65422a7091162aec3ddfdc53
David Thomas Bernal <david.bernal@gmail.com>
2010-07-07 11:44:28 -0700

    added html::chars around output of title in default template. Fixes #2

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -2,10 +2,10 @@
 <html xml:lang="en-us" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title><?= $title ?></title>
+	<title><?= <?= html::chars($title) ?></title>
 </head>
 <body>
-	<h1><?= $title ?></h1>
+	<h1><?= html::chars($title) ?></h1>
 	<div><?= $content ?></div>
 </body>
 </html>
