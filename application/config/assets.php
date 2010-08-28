11514770cf41660f83534593a5f8b773a1df8aef
Lorenzo Pisani <zeelot3k@gmail.com>
2010-08-27 17:40:09 -0700

    new syntax for defining assets

--- application/config/assets.php
+++ application/config/assets.php
@@ -5,16 +5,16 @@ return array
 	'default-template' => array
 	(
 		// Very important for these 2 to be loaded first
-		array(HTML::style('media/packages/restyle/reset.css'), -100),
-		array(HTML::style('media/packages/restyle/restyle.css'), -90),
+		array('style', 'media/packages/restyle/reset.css', 'head', -100),
+		array('style', 'media/packages/restyle/restyle.css', 'head', -90),
 
-		array(HTML::style('media/css/styles.css')),
-		array(HTML::style('media/packages/notices/css/notices.css')),
-		array(HTML::script('http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js')),
-		array(HTML::script('media/packages/notices/js/notices.js'), 10),
+		array('style', 'media/css/styles.css', 'head'),
+		array('style', 'media/packages/notices/css/notices.css', 'head'),
+		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js', 'head'),
+		array('script', 'media/packages/notices/js/notices.js', 'body', 10),
 	),
 	'jquery-ui' => array
 	(
-		array(HTML::script('http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js'), 10),
+		array('script', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.2/jquery-ui.min.js', 'body', 10),
 	),
 );
