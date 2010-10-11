88a33a6fb4be15262b7cdab0afe7ebfd4835b79d
Bob Eagan <bob@synapsestudios.com>
2010-10-11 13:44:11 -0700

    Fixing to match coding standards and adding section for naming sessions of native type with todo to replace placeholder data

--- application/config/session.php
+++ application/config/session.php
@@ -1,11 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-return array(
-	'database' => array(
+return array
+(
+	'native' => array
+	(
+		//@TODO change this to be project-specific
+		'name' => 'project_template',
+	),
+	'database' => array
+	(
 		'group' => 'default',
 		'table' => 'sessions',
 	),
-	'cookie' => array(
+	'cookie' => array
+	(
 		'encrypted' => TRUE,
 	),
 );
