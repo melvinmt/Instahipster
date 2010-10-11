0bedbea7833e77f381b8d65d55c22d6ad04b074e
Bob Eagan <bob@synapsestudios.com>
2010-10-11 13:42:25 -0700

    Removing excess data from database config and adding todo to change placeholder data

--- application/config/database.php
+++ application/config/database.php
@@ -5,44 +5,13 @@ return array
 	'default' => array
 	(
 		'type'       => 'mysql',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname
-			 * integer  port
-			 * string   socket
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   database
-			 */
+		'connection' => array
+		(
+			//@TODO change this to be project-specific
 			'hostname'   => 'localhost',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
-			'database'   => 'kohana',
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-		'profiling'    => TRUE,
-	),
-	'alternate' => array(
-		'type'       => 'pdo',
-		'connection' => array(
-			/**
-			 * The following options are available for PDO:
-			 *
-			 * string   dsn
-			 * string   username
-			 * string   password
-			 * boolean  persistent
-			 * string   identifier
-			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
-			'username'   => 'root',
-			'password'   => 'r00tdb',
+			'username'   => 'user_name',
+			'password'   => 'password',
+			'database'   => 'db_name',
 			'persistent' => FALSE,
 		),
 		'table_prefix' => '',
