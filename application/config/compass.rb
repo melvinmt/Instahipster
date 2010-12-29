26a5e0b6fe9c7a45554179d4e12533260dfb1f48
Lorenzo Pisani <zeelot3k@gmail.com>
2010-12-29 15:20:30 -0500

    moving the compass config to application/config

---
+++ application/config/compass.rb
@@ -0,0 +1,4 @@
+project_type = :stand_alone
+css_dir = "media/css/compiled"
+sass_dir = "media/css/scss"
+output_style = :compressed
--- config/compass.rb
+++
@@ -1,4 +0,0 @@
-project_type = :stand_alone
-css_dir = "application/media/css/compiled"
-sass_dir = "application/media/css/scss"
-output_style = :compressed
