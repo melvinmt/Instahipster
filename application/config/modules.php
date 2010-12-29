5af3e0fd01b766862338f9bd23ef611ac1be9bcb
Lorenzo Pisani <zeelot3k@gmail.com>
2010-12-29 14:04:59 -0500

    updating the modules config

--- application/config/modules.php
+++ application/config/modules.php
@@ -2,23 +2,18 @@
 
 return array(
 	'acl'                => MODPATH.'acl',
-	'assets'             => MODPATH.'assets',
 	'auth'               => MODPATH.'auth',
-	'codebench'          => MODPATH.'codebench',
 	'csrf'               => MODPATH.'csrf',
 	'database'           => MODPATH.'database',
 	'doctrinemigrations' => MODPATH.'doctrinemigrations',
 	'email'              => MODPATH.'email',
 	'errors'             => MODPATH.'errors',
 	'image'              => MODPATH.'image',
-	'jssettings'         => MODPATH.'jssettings',
 	'kostache'           => MODPATH.'kostache',
-	'less'               => MODPATH.'less',
 	'notices'            => MODPATH.'notices',
 	'orm'                => MODPATH.'orm',
 	'pagination'         => MODPATH.'pagination',
 	'unittest'           => MODPATH.'unittest',
 	'userguide'          => MODPATH.'userguide',
 	'yform'              => MODPATH.'yform',
-	'config.json'        => MODPATH.'config.json',
 );
