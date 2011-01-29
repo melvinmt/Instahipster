0593bc298fb4e49b0cad31ad68da4502e51d3af9
Lorenzo Pisani <zeelot3k@gmail.com>
2011-01-29 01:02:06 -0700

    fixing config file for the repo minion task

--- application/config/minion-repo-forks.php
+++ application/config/minion-repo-forks.php
@@ -1,47 +1,56 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	MODPATH.'auth' => array(
+	'modules/auth' => array(
+		'path'       => MODPATH.'auth',
 		'fetch_from' => 'https://github.com/kohana/auth.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-auth.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'database' => array(
+	'modules/database' => array(
+		'path'       => MODPATH.'database',
 		'fetch_from' => 'https://github.com/kohana/database.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-database.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'image' => array(
+	'modules/image' => array(
+		'path'       => MODPATH.'image',
 		'fetch_from' => 'https://github.com/kohana/image.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-image.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'minion' => array(
+	'modules/minion' => array(
+		'path'       => MODPATH.'minion',
 		'fetch_from' => 'https://github.com/BRMatt/kohana-minion.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-minion.git',
 		'checkout'   => 'origin/develop',
 	),
-	MODPATH.'orm' => array(
+	'modules/orm' => array(
+		'path'       => MODPATH.'orm',
 		'fetch_from' => 'https://github.com/kohana/orm.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-orm.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'unittest' => array(
+	'modules/unittest' => array(
+		'path'       => MODPATH.'unittest',
 		'fetch_from' => 'https://github.com/kohana/unittest.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-unittest.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'userguide' => array(
+	'modules/userguide' => array(
+		'path'       => MODPATH.'userguide',
 		'fetch_from' => 'https://github.com/kohana/userguide.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-userguide.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'git' => array(
+	'modules/git' => array(
+		'path'       => MODPATH.'git',
 		'fetch_from' => 'https://github.com/Zeelot/kohana-git.git',
 		'push_to'    => 'git@github.com:synapsestudios/kohana-git.git',
 		'checkout'   => 'origin/3.1/develop',
 	),
-	MODPATH.'minion-tasks-repo' => array(
+	'modules/minion-tasks-repo' => array(
+		'path'       => MODPATH.'minion-tasks-repo',
 		'fetch_from' => 'https://github.com/Zeelot/minion-tasks-repo.git',
 		'push_to'    => 'git@github.com:synapsestudios/minion-tasks-repo.git',
 		'checkout'   => 'origin/3.1/develop',
