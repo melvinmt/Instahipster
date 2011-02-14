94ac33b67ba7637547d3e95835eab991aeb3dd03
Bob Eagan <bob@synapsestudios.com>
2011-02-14 16:15:09 -0700

    updating production sessions save path to be a better template for what should be there

--- application/config/session.php
+++ application/config/session.php
@@ -2,7 +2,7 @@
 
 return array(
 	// @TODO change this to be environment-specific and override production value in environment config
-	'save_path' => DOCROOT.'../../../sessions',
+	'save_path' => '/var/www/project_template/production/shared/sessions',
 	'native' => array(
 		// @TODO change this to be project-specific
 		'name' => 'project_template',
