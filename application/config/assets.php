25f7f78bde9862bfc99c4a9a6191a3213d6ab501
Lorenzo Pisani <zeelot3k@gmail.com>
2011-04-24 12:29:08 -0700

    updating asset path

--- application/config/assets.php
+++ application/config/assets.php
@@ -4,7 +4,7 @@ return array
 (
 	'default-template' => array
 	(
-		array('style', 'media/ko/css/compiled/styles/'.Kohana::APP_VERSION.'.css', 'head'),
+		array('style', 'media/'.Kohana::APP_VERSION.'/kohana/css/compiled/styles.css', 'head'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js', 'head'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js', 'body', 10),
 	),
