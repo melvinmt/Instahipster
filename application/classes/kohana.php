5316e2eddd8dd19b395e1905088380d59044a559
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-24 12:33:54 -0700

    adding Kohana::app_revision()

--- application/classes/kohana.php
+++ application/classes/kohana.php
@@ -5,4 +5,17 @@ class Kohana extends Kohana_Core
 	const APP_VERSION = '0.0.0';
 	const QA = 5;
 	const BETA = 6;
+
+	public static function app_revision()
+	{
+		static $revision;
+
+		if ( ! $revision)
+		{
+			$git = new Git(APPPATH);
+			$revision = $git->execute('log --pretty=format:\'%H\' -1');
+		}
+
+		return $revision;
+	}
 }
