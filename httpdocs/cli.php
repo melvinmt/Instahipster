6963a3b82d9b8c9cf30b9894966757be1e5c181b
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-24 12:55:46 -0700

    we no longer need cli.php because we use Minion now. (refs #30)

--- httpdocs/cli.php
+++
@@ -1,8 +0,0 @@
-<?php PHP_SAPI === 'cli' or die('No direct script access.');
-
-// This is usually defined in the VHOST so we need it also done in the crons
-$arguments = getopt("e:");
-putenv('ENVIRONMENT='.$arguments['e']);
-
-// Continue the normal Kohana execution
-require 'index.php';
