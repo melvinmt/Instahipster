5c8dab1e8284c18724e7d845239e41b3c982c540
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-30 16:08:47 -0700

    refactoring the app:watch task a bit

--- application/classes/minion/task/app/watch.php
+++ application/classes/minion/task/app/watch.php
@@ -4,29 +4,42 @@ class Minion_Task_App_Watch extends Minion_Task {
 
 	public function execute(array $config)
 	{
-		$watch_path = APPPATH.'media/js';
+		$watch_path = APPPATH.'media/js/app';
 		$events = '-e modify -e move -e create -e delete';
-		$cmd = 'inotifywait '.$events.' -r -q --format \'%w%f\' "'.$watch_path.'"';
+		$compile_dir = APPPATH.'media/js/app/compiled/';
+		$cmd = 'inotifywait '.$events.' -r -q --format \'%w%f\' @"'.$compile_dir.'" "'.$watch_path.'"';
+
+		// Compile right away
+		$this->compile();
+
+		Minion_CLI::write('Watching: '.$watch_path);
 		while ($altered = exec($cmd))
 		{
 			Minion_CLI::write($altered);
-
-			// Concat all the files
-			$files = Kohana::list_files('media/js/app');
-			$contents = $this->recursive_concat_contents($files, 'js');
-
-			$tmp_path = APPPATH.'media/js/app.components.js.tmp';
-			$components_path = APPPATH.'media/js/app.components.js';
-			$app_path = APPPATH.'media/js/app.js';
-			$app_compiled_path = APPPATH.'media/js/app.min.js';
-
-			file_put_contents($tmp_path, $contents);
-			file_put_contents($components_path, exec('uglifyjs "'.$tmp_path.'"'));
-			file_put_contents($app_compiled_path, exec('uglifyjs "'.$app_path.'"'));
-
-			unlink($tmp_path);
-			sleep(1);
+			$this->compile();
 		}
+	}
+
+	protected function compile()
+	{
+		// Concat all the files
+		$files = Kohana::list_files('media/js/app/components');
+		$contents = $this->recursive_concat_contents($files, 'js');
+
+		$compile_dir = APPPATH.'media/js/app/compiled/';
+
+		// The components without minification (use in dev environments)
+		$concat_path = $compile_dir.'app.components.js';
+		// Minified components for production
+		$components_path = $compile_dir.'app.components.min.js';
+		// For dev environment
+		$app_path = APPPATH.'media/js/app/app.js';
+		// Minified app for production
+		$app_compiled_path = $compile_dir.'app.min.js';
+
+		file_put_contents($concat_path, $contents);
+		file_put_contents($components_path, exec('uglifyjs "'.$concat_path.'"'));
+		file_put_contents($app_compiled_path, exec('uglifyjs "'.$app_path.'"'));
 	}
 
 	protected function recursive_concat_contents(array $files, $ext)
