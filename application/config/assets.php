0e3c29bd3d432444e65ad63736f03215c695b3a4
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-29 17:44:58 -0700

    moving jquery to the body section

--- application/config/assets.php
+++ application/config/assets.php
@@ -5,7 +5,7 @@ return array
 	'default-template' => array
 	(
 		array('style', Media::url('css/compiled/styles.css'), 'head'),
-		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js', 'head'),
+		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js', 'body'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js', 'body', 10),
 	),
 );
