63b5eb24286493e19782f5fb557d1fa9eb1109a0
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-30 16:25:32 -0700

    fixing asset weights

--- application/config/assets.php
+++ application/config/assets.php
@@ -7,9 +7,9 @@ return array
 		array('style', Media::url('css/compiled/styles.css'), 'head'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js', 'body'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js', 'body', 10),
-		array('script', Media::url('js/underscore-min.js'), 'body'),
-		array('script', Media::url('js/backbone-min.js'), 'body'),
-		array('script', Media::url('js/app/compiled/app.components.min.js'), 'body'),
-		array('script', Media::url('js/app/compiled/app.min.js'), 'body'),
+		array('script', Media::url('js/underscore-min.js'), 'body', 10),
+		array('script', Media::url('js/backbone-min.js'), 'body', 20),
+		array('script', Media::url('js/app/compiled/app.components.min.js'), 'body', 30),
+		array('script', Media::url('js/app/compiled/app.min.js'), 'body', 40),
 	),
 );
