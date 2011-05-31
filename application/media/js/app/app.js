bac6236c4248093f18bb99f7709fd3e8d166bf88
Lorenzo Pisani <zeelot3k@gmail.com>
2011-05-30 17:28:55 -0700

    initializing the app based on the body id. <body id="page_main_index" will look for app.controller.page_main_index.init() and run it

--- application/media/js/app/app.js
+++ application/media/js/app/app.js
@@ -1,3 +1,11 @@
 (function() {
 	// Initialize the app here
+	$(function() {
+		var body_id = $('body').attr('id');
+
+		if (app.controller[body_id].init)
+		{
+			app.controller[body_id].init();
+		}
+	});
 })();
