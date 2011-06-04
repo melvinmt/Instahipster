a6cc28a4da09a8266a98bc93dfb5c74128ea44c9
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-04 11:09:11 -0700

    fixing the app:watch task to concat templates as well

--- application/classes/minion/task/app/watch.php
+++ application/classes/minion/task/app/watch.php
@@ -23,25 +23,29 @@ class Minion_Task_App_Watch extends Minion_Task {
 	protected function compile()
 	{
 		// Start with the bootstrap at the top of the file
-		$contents = file_get_contents(APPPATH.'media/js/app/bootstrap.js');
+		$js = file_get_contents(APPPATH.'media/js/app/bootstrap.js');
 
 		// Concat all the component files (MVC)
 		$files = Kohana::list_files('media/js/app/components');
-		$contents .= $this->recursive_concat_contents($files, 'js');
+		$js .= $this->recursive_concat_contents($files, 'js');
+		$views = $this->recursive_concat_contents($files, 'mustache');
 
 		// Append the app.js file so it runs last
-		$contents .= "\n".file_get_contents(APPPATH.'media/js/app/app.js');
+		$js .= "\n".file_get_contents(APPPATH.'media/js/app/app.js');
 
 		$compile_dir = APPPATH.'media/js/app/compiled/';
 
-		// The components without minification (use in dev environments)
-		$concat_path = $compile_dir.'app.js';
-		// Minified components for production
-		$components_path = $compile_dir.'app.min.js';
+		// The JS without minification (use in dev environments)
+		$js_concat_path = $compile_dir.'app.js';
+		// Minified JS for production
+		$js_min_path = $compile_dir.'app.min.js';
+		// Concatinated views templates
+		$views_concat_path = $compile_dir.'templates.mustache';
 
-		file_put_contents($concat_path, $contents);
+		file_put_contents($js_concat_path, $js);
+		file_put_contents($views_concat_path, $views);
 		// Not mangling variable names and not removing unused code
-		file_put_contents($components_path, exec('uglifyjs --no-mangle --no-dead-code "'.$concat_path.'"'));
+		file_put_contents($js_min_path, exec('uglifyjs --no-mangle --no-dead-code "'.$js_concat_path.'"'));
 	}
 
 	protected function recursive_concat_contents(array $files, $ext)
