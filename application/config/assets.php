3c3141e36497b8d715903c8b1d31ca04512afb78
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-04 11:02:45 -0700

    adding mustache.js to the project and using the non minified app.js in development for easier debugging

--- application/config/assets.php
+++ application/config/assets.php
@@ -1,5 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+$js_app_file = Kohana::$environment === Kohana::DEVELOPMENT
+	? 'js/app/compiled/app.js'
+	: 'js/app/compiled/app.min.js';
+
 return array
 (
 	'default-template' => array
@@ -7,8 +11,9 @@ return array
 		array('style', Media::url('css/compiled/styles.css'), 'head'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js', 'body'),
 		array('script', 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js', 'body', 10),
+		array('script', Media::url('js/mustache.js'), 'body', 10),
 		array('script', Media::url('js/underscore-min.js'), 'body', 10),
 		array('script', Media::url('js/backbone-min.js'), 'body', 20),
-		array('script', Media::url('js/app/compiled/app.min.js'), 'body', 40),
+		array('script', Media::url($js_app_file), 'body', 40),
 	),
 );
