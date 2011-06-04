ebf05f4009a70f6cd16c4f28030748e2fd3f2e38
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-04 16:09:04 -0700

    projecttemplate dev doesn't need to be on ssl

--- application/config/environments/development/init.php
+++ application/config/environments/development/init.php
@@ -2,7 +2,7 @@
 
 return array(
 	// @TODO: change this to be project-specific
-	'base_url'    => 'https://dev.vm/projecttemplate/',
+	'base_url'    => 'http://dev.vm/projecttemplate/',
 	'index_file'  => '',
 	'charset'     => 'utf-8',
 	'cache_dir'   => APPPATH.'cache',
