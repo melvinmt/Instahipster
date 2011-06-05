b705cff7edd42343282c60cdc005a09b986f7f41
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-04 18:34:30 -0700

    appending the templates to the body

--- application/media/js/app/app.js
+++ application/media/js/app/app.js
@@ -1,11 +1,17 @@
 (function() {
 	// Initialize the app here
 	$(function() {
-		var body_id = $('body').attr('id');
+		var $body = $('body');
+		var body_id = $body.attr('id');
 
 		if (app.controller[body_id] && app.controller[body_id].init)
 		{
-			app.controller[body_id].init();
+			$.ajax(Kohana.media_url + '/js/app/compiled/templates.mustache',{
+				success: function(response){
+					$body.append(response);
+					app.controller[body_id].init();
+				}
+			});
 		}
 	});
 })();
