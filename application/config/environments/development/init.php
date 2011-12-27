4c7ec75db27993f918e7fd727e7248194e4d8e85
Lorenzo Pisani <zeelot3k@gmail.com>
2011-12-27 11:31:46 -0800

    updating the dev init config to use localhost:8080

--- application/config/environments/development/init.php
+++ application/config/environments/development/init.php
@@ -2,7 +2,7 @@
 
 return array(
 	// @TODO: change this to be project-specific
-	'base_url'    => 'http://dev.vm/projecttemplate/',
+	'base_url'    => 'http://localhost:8080/',
 	'index_file'  => '',
 	'charset'     => 'utf-8',
 	'cache_dir'   => APPPATH.'cache',
