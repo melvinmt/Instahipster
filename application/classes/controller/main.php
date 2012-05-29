9e665d5eb4b93de610a32937a5b7d9790932aade
Lorenzo Pisani <zeelot3k@gmail.com>
2012-05-28 20:01:03 -0700

    extend new page controller

--- application/classes/controller/main.php
+++ application/classes/controller/main.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Main extends Abstract_Controller_Website {
+class Controller_Main extends Abstract_Controller_Page {
 
 	public function action_index() {}
 
